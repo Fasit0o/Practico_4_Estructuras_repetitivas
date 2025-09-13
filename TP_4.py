7b6166cdafd9e8347c6ef4c742f5d8308e77af08
Fasit0o <sotofranco12348@gmail.com>
2025-09-13 20:40:28 -0300

    Update TP_4.py

--- TP_4.py
+++ TP_4.py
@@ -19,12 +19,12 @@ num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 
 if num1 > num2:
-    num1, num2 = num2, num1  # intercambiamos los valores
+    num1, num2 = num2, num1 
 
 suma = 0
 
 for i in range(num1 + 1, num2):
-    suma += i  # vamos sumando cada número
+    suma += i  
 
 print("La suma de los números comprendidos entre", num1, "y", num2, "es:", suma)
 
@@ -65,14 +65,73 @@ for i in range(0, 101):
 #7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
 #número entero positivo indicado por el usuario.
 
+n = int(input("Ingrese un número entero positivo: "))
+suma = 0
+
+for i in range(n + 1):
+    suma += i 
+
+print("La suma de los números desde 0 hasta", n, "es:", suma)
+
+
 #8) Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el
 #programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
 #negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
 #menor, pero debe estar preparado para procesar 100 números con un solo cambio).
 
+contador = 0
+contador_par = 0
+contador_impar = 0
+contador_negativo = 0
+contador_positivo = 0
+
+while contador < 100:
+    numero = int(input("Ingrese un número entero: "))
+    contador += 1
+    if numero % 2 == 0:
+        contador_par += 1
+    else:
+        contador_impar += 1
+
+    if numero < 0:
+        contador_negativo += 1
+    else:
+        contador_positivo += 1
+
+print("Números pares:", contador_par)
+print("Números impares:", contador_impar)
+print("Números negativos:", contador_negativo)
+print("Números positivos:", contador_positivo)
+
+
 #9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
 #media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
 #poder procesar 100 números cambiando solo un valor).
 
+
+cantidad = 100 
+suma = 0
+
+for i in range(cantidad):
+    numero = int(input(f"Ingrese el número {i+1}: "))
+    suma += numero  
+
+media = suma / cantidad
+
+print("La media de los números ingresados es:", media)
+
+
 #10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
 #usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745
+
+numero = int(input("Ingrese un número entero: "))
+
+signo = -1 if numero < 0 else 1
+
+numero_str = str(abs(numero))
+
+numero_invertido_str = numero_str[::-1]
+
+numero_invertido = int(numero_invertido_str) * signo
+
+print("El número invertido es:", numero_invertido)
