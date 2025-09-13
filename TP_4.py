35d38fa0e654d74b792dd428e51d33f91effea09
Fasit0o <sotofranco12348@gmail.com>
2025-09-13 19:57:24 -0300

    Update TP_4.py

--- TP_4.py
+++ TP_4.py
@@ -1,19 +1,66 @@
 #1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
 #(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
 
+for i in range(101):
+    print(i)
+
 #2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de dígitos que contiene.
+
+numero = int(input("Ingrese un número entero: "))
+numero_abs = abs(numero)
+cantidad_digitos = len(str(numero_abs))
+print("El número tiene", cantidad_digitos, "dígitos.")
+
 
 #3) Escribe un programa que sume todos los números enteros comprendidos entre dos valores
 #dados por el usuario, excluyendo esos dos valores.
 
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+if num1 > num2:
+    num1, num2 = num2, num1  # intercambiamos los valores
+
+suma = 0
+
+for i in range(num1 + 1, num2):
+    suma += i  # vamos sumando cada número
+
+print("La suma de los números comprendidos entre", num1, "y", num2, "es:", suma)
+
+
 #4) Elabora un programa que permita al usuario ingresar números enteros y los sume en
 #secuencia. El programa debe detenerse y mostrar el total acumulado cuando el usuario ingrese un 0.
+
+suma = 0
+num = 1
+while num != 0:
+    num = int(input("Ingrese un número entero: "))
+    suma += num
+
+print("El total acumulado es:", suma)
+
 
 #5) Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el
 #programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
 
+import random
+
+num = random.randint(0, 9)
+intentos = 0
+
+while num != int(input("Ingrese un número: ")):
+    intentos += 1
+    num = random.randint(0, 9)
+
+print("El número era", num, "y tus intentos fueron", intentos)
+
 #6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
 #entre 0 y 100, en orden decreciente.
+
+for i in range(0, 101):
+    pares = i % 2
+    print(pares)
 
 #7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
 #número entero positivo indicado por el usuario.
